4b51c0886b9b46a9da1fce15fd54f0f2ee75ac66
mtintes <michael.tintes@hotmail.com>
2024-09-17 21:17:24 -0500

    clean up descriptions

--- cli/cmd/key.go
+++ cli/cmd/key.go
@@ -13,13 +13,8 @@ import (
 // keyCmd represents the key command
 var keyCmd = &cobra.Command{
 	Use:   "key",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Find the value of a single key",
+	Long:  `This is for finding the value of a single key`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		key := ""

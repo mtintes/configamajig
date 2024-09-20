2315f8c76cf112c17ff357d792d0a8a44133e186
mtintes <michael.tintes@hotmail.com>
2024-09-19 20:52:17 -0500

    adds sprig to templating for even more funcs

--- cli/actions/templating.go
+++ cli/actions/templating.go
@@ -3,17 +3,16 @@ package actions
 import (
 	"strings"
 	"text/template"
+
+	"github.com/Masterminds/sprig/v3"
 )
 
 func RunTemplate(inputFile []byte, memoryMap map[string]interface{}) (string, error) {
 
-	tmpl, err := template.New("replace").Parse(string(inputFile))
+	tmpl := template.Must(template.New("replace").Funcs(sprig.FuncMap()).Parse(string(inputFile)))
 
-	if err != nil {
-		return "", err
-	}
 	b := new(strings.Builder)
-	err = tmpl.Execute(b, memoryMap)
+	err := tmpl.Execute(b, memoryMap)
 	if err != nil {
 		return "", err
 	}
